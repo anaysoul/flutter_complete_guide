8e8650cb0b4736c6728198b81780273bea23e75b
anaysoul <soulyana_lakew@outlook.com>
2022-02-25 19:54:07 -0500

    running an app on the emulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello World!'),);
+    return MaterialApp(
+      home: Text('Hello World!'),
+    );
   }
 }
