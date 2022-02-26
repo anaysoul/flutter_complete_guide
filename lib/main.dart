eee2ceba22d1a1c4d34cfe824f5c32dfe0a429af
anaysoul <soulyana_lakew@outlook.com>
2022-02-25 20:09:14 -0500

    first summary and additional syntax

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override // decorator provided by dart 
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text('Hello World!'),
