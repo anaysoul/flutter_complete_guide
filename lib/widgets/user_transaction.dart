8095d9ef2e525baaf47b7ee937c44b4cfde7d0de
anaysoul <soulyana_lakew@outlook.com>
2022-02-27 00:48:45 -0500

    connecting widgets & managing data / state

--- lib/widgets/user_transaction.dart
+++ lib/widgets/user_transaction.dart
@@ -9,16 +9,31 @@ class UserTransactions extends StatefulWidget {
   _UserTransactionsState createState() => _UserTransactionsState();
 }
 
-final List<Transaction> _userTransaction = [
-  Transaction(
-      id: 't1', title: 'Yeezy Shoes', amount: 169.99, date: DateTime.now()),
-  Transaction(
-      id: 't2', title: 'Groceries', amount: 350.99, date: DateTime.now())
-];
-
 class _UserTransactionsState extends State<UserTransactions> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+        id: 't1', title: 'Yeezy Shoes', amount: 169.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Groceries', amount: 350.99, date: DateTime.now())
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[NewTransaction(), TransactionList()]);
+    return Column(children: <Widget>[
+      NewTransaction(),
+      TransactionList(_userTransactions)
+    ]);
   }
 }
