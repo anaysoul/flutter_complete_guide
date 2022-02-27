7a06c718286e394ddec63a90c022732a4e44cf8a
anaysoul <soulyana_lakew@outlook.com>
2022-02-27 01:02:56 -0500

    making the list scrollable

--- lib/main.dart
+++ lib/main.dart
@@ -26,19 +26,21 @@ class MyHomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter App'),
         ),
-        body: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  child: Text('CHART!'),
-                  elevation: 5,
+        body: SingleChildScrollView(
+          child: Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  child: Card(
+                    color: Colors.blue,
+                    child: Text('CHART!'),
+                    elevation: 5,
+                  ),
                 ),
-              ),
-              UserTransactions(),
-            ]));
+                UserTransactions(),
+              ]),
+        ));
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,41 +10,46 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-            child: Row(children: <Widget>[
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.deepPurple, width: 2)),
-            child: Text(
-              '\$${tx.amount}',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.purple,
-              ),
-            ),
-            padding: EdgeInsets.all(10),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(tx.title,
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((tx) {
+            return Card(
+                child: Row(children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.deepPurple, width: 2)),
+                child: Text(
+                  '\$${tx.amount}',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  )),
-              Text(
-                DateFormat.yMMMMd().format(tx.date),
-                style: TextStyle(
-                    color: Colors.blueGrey, fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-        ]));
-      }).toList(),
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+                padding: EdgeInsets.all(10),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(tx.title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      )),
+                  Text(
+                    DateFormat.yMMMMd().format(tx.date),
+                    style: TextStyle(
+                        color: Colors.blueGrey, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ]));
+          }).toList(),
+        ),
+      ),
     );
   }
 }
