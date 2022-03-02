3761497f249b739db5a1ba2a419dbbe8f12d115c
anaysoul <soulyana_lakew@outlook.com>
2022-03-01 20:28:56 -0500

    using named routes & passing data with named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_screen.dart';
 import './categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -8,20 +9,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
-      theme: ThemeData(
-          primarySwatch: Colors.pink,
-          secondaryHeaderColor: Colors.amber,
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              headline6: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'RobotoCondensed',
-                  fontWeight: FontWeight.bold))),
-      home: CategoriesScreen(),
-    );
+        title: 'DeliMeals',
+        theme: ThemeData(
+            primarySwatch: Colors.pink,
+            secondaryHeaderColor: Colors.amber,
+            canvasColor: Color.fromRGBO(255, 254, 229, 1),
+            fontFamily: 'Raleway',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                headline6: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'RobotoCondensed',
+                    fontWeight: FontWeight.bold))),
+        home: CategoriesScreen(),
+        routes: {
+          '/category-meals': (ctx) =>
+              CategoryMealsScreen(categoryId, categoryTitle)
+        });
   }
 }
