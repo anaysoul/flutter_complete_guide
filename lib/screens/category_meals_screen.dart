d50b99c510218ffd0d7b5167c5e7b292cd913d13
anaysoul <soulyana_lakew@outlook.com>
2022-03-01 21:34:50 -0500

    displaying recipe items & using network images

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
 
-import './dummy_data.dart';
+import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   // final String categoryId;
@@ -23,7 +24,12 @@ class CategoryMealsScreen extends StatelessWidget {
       appBar: AppBar(title: Text('$categoryTitle Recipes')),
       body: ListView.builder(
           itemBuilder: (ctx, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                affordability: categoryMeals[index].affordability,
+                complexity: categoryMeals[index].complexity,
+                duration: categoryMeals[index].duration);
           },
           itemCount: categoryMeals.length),
     );
