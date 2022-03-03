7fa189bb4b649d7a6bd43424e8840e7865f5d60f
anaysoul <soulyana_lakew@outlook.com>
2022-03-03 16:18:59 -0500

    Adding navigation to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
@@ -7,12 +8,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-          primarySwatch: Colors.lightGreen,
-          secondaryHeaderColor: Colors.deepOrange,
-          fontFamily: 'Lato'),
-      home: ProductsOverviewScreen(),
-    );
+        title: 'MyShop',
+        theme: ThemeData(
+            primarySwatch: Colors.lightGreen,
+            secondaryHeaderColor: Colors.deepOrange,
+            fontFamily: 'Lato'),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()
+        });
   }
 }
