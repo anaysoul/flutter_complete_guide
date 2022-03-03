521ae2ae1bc526ac0adf9e1092c481d70ab3a4d7
anaysoul <soulyana_lakew@outlook.com>
2022-03-03 15:11:52 -0500

    starting the shop app with state mangement

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,30 @@
 import 'package:flutter/material.dart';
-import './dummy_data.dart';
-import './screens/filters_screen.dart';
-import './screens/tabs_screen.dart';
-import './screens/meal_detail_screen.dart';
-import './screens/category_meals_screen.dart';
-import './screens/categories_screen.dart';
-import './models/meal.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Map<String, bool> _filters = {
-    'gluten': false,
-    'lactose': false,
-    'vegan': false,
-    'vegetarian': false
-  };
-
-  List<Meal> _availableMeals = DUMMY_MEALS;
-  List<Meal> _favoriteMeals = [];
-
-  void _setFilters(Map<String, bool> filterData) {
-    setState(() {
-      _filters = filterData;
-      _availableMeals = DUMMY_MEALS.where((meal) {
-        if (_filters['gluten'] && !meal.isGlutenFree) {
-          return false;
-        }
-        if (_filters['lactose'] && !meal.isLactoseFree) {
-          return false;
-        }
-        if (_filters['vegan'] && !meal.isVegan) {
-          return false;
-        }
-        if (_filters['vegetarian'] && !meal.isVegetarian) {
-          return false;
-        }
-        return true;
-      }).toList();
-    });
-  }
-
-  void _toggleFavorite(String mealId) {
-    final exisitingIndex =
-        _favoriteMeals.indexWhere((meal) => meal.id == mealId);
-    if (exisitingIndex >= 0) {
-      setState(() {
-        _favoriteMeals.removeAt(exisitingIndex);
-      });
-    } else {
-      setState(() {
-        _favoriteMeals.add(DUMMY_MEALS.firstWhere((meal) => meal.id == mealId));
-      });
-    }
-  }
-
-  bool _isMealFavorite(String id) {
-    return _favoriteMeals.any((meal) => meal.id == id);
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SOULFOOD',
+      title: 'MyShop',
       theme: ThemeData(
-          primarySwatch: Colors.pink,
-          secondaryHeaderColor: Colors.amber,
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              headline6: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'RobotoCondensed',
-                  fontWeight: FontWeight.bold))),
-      // home: CategoriesScreen(),
-      initialRoute: '/',
-      routes: {
-        '/': (ctx) => TabsScreen(_favoriteMeals),
-        CategoryMealsScreen.routeName: (ctx) =>
-            CategoryMealsScreen(_availableMeals),
-        MealDetailScreen.routeName: (ctx) =>
-            MealDetailScreen(_toggleFavorite, _isMealFavorite),
-        FiltersScreen.routeName: (ctx) => FiltersScreen(_filters, _setFilters)
-      },
-      // for dynamically created routes (more complex projects!)
-      // fallback option as well
-      // ignore: missing_return
-      onGenerateRoute: (settings) {
-        print(settings.arguments);
-        //   return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
-      },
-      // fallback route
-      onUnknownRoute: (settings) {
-        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
-      },
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: Center(
+        child: Text('Let\'s build a shop!'),
+      ),
     );
   }
 }
