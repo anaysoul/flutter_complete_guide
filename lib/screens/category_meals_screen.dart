e33665b4ca93a7cfefbb6db8db09e8839362d28b
anaysoul <soulyana_lakew@outlook.com>
2022-03-03 14:58:56 -0500

    completed meal app

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -43,12 +43,6 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
     super.didChangeDependencies();
   }
 
-  void _removeMeal(String mealId) {
-    setState(() {
-      displayedMeals.removeWhere((meal) => meal.id == mealId);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
