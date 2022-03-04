2864cf2c39b614d92a2a2e246ec5fdceb063a203
anaysoul <soulyana_lakew@outlook.com>
2022-03-04 13:08:53 -0500

    using a side drawer

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
+import '../widgets/app_drawer.dart';
 import '../screens/cart_screen.dart';
 import '../providers/cart.dart';
 
@@ -56,6 +57,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           )
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
