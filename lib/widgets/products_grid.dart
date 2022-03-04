69ae723461ab857c7cdaa190af5756d0b24b9980
anaysoul <soulyana_lakew@outlook.com>
2022-03-03 20:40:08 -0500

    using nested models & providers

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -16,8 +16,11 @@ class ProductsGrid extends StatelessWidget {
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10),
-      itemBuilder: (ctx, i) =>
-          ProductItem(products[i].id, products[i].title, products[i].imageUrl),
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+          create: (c) => products[i],
+          child: ProductItem(
+              // products[i].id, products[i].title, products[i].imageUrl
+              )),
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
     );
