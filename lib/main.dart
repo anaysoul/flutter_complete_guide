05470f3bd2df66e0ab60281f56cabd9831d6907c
anaysoul <soulyana_lakew@outlook.com>
2022-03-04 12:12:27 -0500

    providing an orders object

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import './screens/products_overview_screen.dart';
 import './screens/cart_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
+import './providers/orders.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,7 +16,8 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (ctx) => Products()),
-        ChangeNotifierProvider(create: (ctx) => Cart())
+        ChangeNotifierProvider(create: (ctx) => Cart()),
+        ChangeNotifierProvider(create: (ctx) => Orders())
       ],
       child: MaterialApp(
           title: 'MyShop',
